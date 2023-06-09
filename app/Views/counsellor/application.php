01d21ccc41c89ac240c84cc305a543a33c536b76
POOJAN-PJ <poojanpatel579@gmail.com>
2023-06-09 11:18:51 +0530

    lead update changes and section change

--- app/Views/counsellor/application.php
+++ app/Views/counsellor/application.php
@@ -75,37 +75,37 @@
 
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="/counsellor/dashboard" class="brand-link">
-        <img src="<?php echo base_url() ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Visa consultant</span>
-      </a>
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="<?php echo base_url() ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-          </div>
-          <div class="info">
-            <a href="#" class="d-block">Shail Shah</a>
-          </div>
-        </div>
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
+            <!-- Brand Logo -->
+            <a href="/counsellor/dashboard" class="brand-link">
+                <img src="<?php echo base_url() ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">Visa consultant</span>
+            </a>
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="<?php echo base_url() ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">Shail Shah</a>
+                    </div>
+                </div>
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
 
-            <li class="nav-item">
-              <a href="/counsellor/Leads" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Leads
-                </p>
-              </a>
-            </li>
-            <!-- <li class="nav-item">
+                        <li class="nav-item">
+                            <a href="/counsellor/Leads" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Leads
+                                </p>
+                            </a>
+                        </li>
+                        <!-- <li class="nav-item">
               <a href="/universityApplication.html" class="nav-link">
                 <i class="nav-icon fas fa-th"></i>
                 <p>
@@ -113,70 +113,70 @@
                 </p>
               </a>
             </li> -->
-            </li>
-            <li class="nav-item">
-              <a href="/counsellor/CollegeSearch" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  College Search
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/counsellor/AddStudent" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Add Student
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/counsellor/StudentInfo" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Student Information
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/counsellor/VisaDetails" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Visa Details
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/counsellor/Application" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Student Application
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/counsellor/Leave" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Leave
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/counsellor/StudentSearch" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Student search
-                </p>
-              </a>
-            </li>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/counsellor/CollegeSearch" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    College Search
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/counsellor/AddStudent" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Add Student
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/counsellor/StudentInfo" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Student Information
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/counsellor/VisaDetails" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Visa Details
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/counsellor/Application" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Student Application
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/counsellor/Leave" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Leave
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/counsellor/StudentSearch" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Student search
+                                </p>
+                            </a>
+                        </li>
 
-          </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
@@ -545,63 +545,66 @@
                             </table>
                         </div>
                     </div>
+                    <div class="myDiv" id="showvisa">
+                        <section class="content">
+                            <div class="container">
+                                <div class="row">
+                                    <!-- left column -->
+                                    <div class="col">
+                                        <!-- general form elements -->
+                                        <div class="card card-primary">
+                                            <div class="card-header">
+                                                <h3 class="card-title">Visa details</h3>
+                                            </div>
+                                            <form>
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <label for="name">Application ID</label>
+                                                                <input type="text" class="form-control" id="name" placeholder="Enter name">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="Country">Country</label>
+                                                                <select class="form-select">
+                                                                    <option value="canada">Canada</option>
+                                                                    <option value="USA">USA</option>
+                                                                    <option value="UK">UK</option>
+                                                                    <option value="Australia">Australia</option>
+                                                                </select>
+                                                            </div>
 
-                    <section class="content">
-                        <div class="container">
-                            <div class="row">
-                                <!-- left column -->
-                                <div class="col">
-                                    <!-- general form elements -->
-                                    <div class="card card-primary">
-                                        <div class="card-header">
-                                            <h3 class="card-title">Visa details</h3>
-                                        </div>
-                                        <form>
-                                            <div class="card-body">
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="name">Application ID</label>
-                                                            <input type="text" class="form-control" id="name" placeholder="Enter name">
                                                         </div>
-                                                        <div class="form-group">
-                                                            <label for="Country">Country</label>
-                                                            <select class="form-select">
-                                                                <option value="canada">Canada</option>
-                                                                <option value="USA">USA</option>
-                                                                <option value="UK">UK</option>
-                                                                <option value="Australia">Australia</option>
-                                                            </select>
-                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <label for="phone number">Fees</label>
+                                                                <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Fees">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="name">Status</label>
+                                                                <input type="radio" id="Pending" name="Pending" value="Pending">
+                                                                <label for="Pending">Pending</label>
+                                                                <input type="radio" id="css" name="Pending" value="Approved">
+                                                                <label for="Approved">Approved</label>
+                                                                <input type="radio" id="javascript" name="Pending" value="Inprogress">
+                                                                <label for="Inprogress">Inprogress</label>
+                                                            </div>
 
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="phone number">Fees</label>
-                                                            <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Fees">
                                                         </div>
-                                                        <div class="form-group">
-                                                            <label for="name">Status</label>
-                                                            <input type="radio" id="Pending" name="Pending" value="Pending">
-                                                            <label for="Pending">Pending</label>
-                                                            <input type="radio" id="css" name="Pending" value="Approved">
-                                                            <label for="Approved">Approved</label>
-                                                            <input type="radio" id="javascript" name="Pending" value="Inprogress">
-                                                            <label for="Inprogress">Inprogress</label>
-                                                        </div>
-
                                                     </div>
                                                 </div>
-                                            </div>
+                                        </div>
+                                        <!-- /.card-body -->
+                                        <div class="card-footer">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                        </form>
                                     </div>
-                                    <!-- /.card-body -->
-                                    <div class="card-footer">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    </div>
-                                    </form>
                                 </div>
                             </div>
-                        </div>
+                        </section>
+                    </div>
+
                 </div>
             </div>
         </div>
